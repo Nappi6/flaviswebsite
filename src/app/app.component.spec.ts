6de5882351b549a108289696bbcc03658bd01345
Nappi6 <flavius_ardelean2002@yahoo.com>
2023-04-22 21:12:14 +0300

    the last final update ever
    yeah

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cloudkitchenweb'`, () => {
+  it(`should have as title 'flaviswebsite'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cloudkitchenweb');
+    expect(app.title).toEqual('flaviswebsite');
   });
 
   it('should render title', () => {
